19972e9e619a39aaef9ff05d5f21597c638567b6
akhil055-mvsr <245122748055@mvsrec.edu.in>
2025-06-08 12:02:13 +0530

    Implemented student details program using java and gonna test it out using jenkins

---
+++ student.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class student{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your Name: ");
+		String s1=sc.nextLine();
+                System.out.println("Enter your dept: ");
+		String s2=sc.nextLine();
+		System.out.println("ENter your rollno:");
+		String r=sc.nextLine();
+		System.out.println("STUDENT DETAILS");
+		System.out.println("Name: "+s1);
+		System.out.println("Department: "+s2);
+		System.out.println("Rollno: "+r);
+	}
+}
