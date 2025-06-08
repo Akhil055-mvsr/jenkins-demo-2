41508d27abcdffe90812a5e768b839b3ae3f9ae4
akhil055-mvsr <245122748055@mvsrec.edu.in>
2025-06-08 12:20:42 +0530

    Update student.java code

--- student.java
+++ student.java
@@ -2,12 +2,9 @@ import java.util.*;
 class student{
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter your Name: ");
-		String s1=sc.nextLine();
-                System.out.println("Enter your dept: ");
-		String s2=sc.nextLine();
-		System.out.println("ENter your rollno:");
-		String r=sc.nextLine();
+		String s1="Akhil";
+		String s2="CSE-AI&ML";
+		String r="2451-22-748-055";
 		System.out.println("STUDENT DETAILS");
 		System.out.println("Name: "+s1);
 		System.out.println("Department: "+s2);
